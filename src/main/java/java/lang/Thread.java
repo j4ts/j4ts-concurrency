157b9d3c77942e6214e565b157e4981cd813e536
Béla Schaum <schaumb@gmail.com>
2018-07-09 04:46:49 +0200

    java.lang.Thread / ThreadGroup with workers

---
+++ src/main/java/java/lang/Thread.java
@@ -0,0 +1,305 @@
+package java.lang;
+
+import def.dom.*;
+import def.js.Array;
+import def.js.Date;
+import def.js.Error;
+import def.js.JSON;
+
+import java.util.function.Function;
+
+import static def.dom.Globals.*;
+import static def.js.Globals.undefined;
+import static jsweet.util.Lang.*;
+
+public class Thread {
+    public static final int MIN_PRIORITY = 0;
+    public static final int NORM_PRIORITY = 0;
+    public static final int MAX_PRIORITY = 0;
+    // thread local variable (each one is)
+    public static final boolean IS_MAIN_THREAD = typeof(window).equals("object");
+    public static final boolean IS_WORKER_THREAD = !IS_MAIN_THREAD && typeof($insert("importScripts")).equals("function");
+
+    public static Thread current;
+    public static Thread parent;
+    public static Array<String> importables;
+    private static int ID_COUNTER = 0;
+
+    static {
+        if (IS_MAIN_THREAD) {
+            current = new Thread(ThreadGroup.getMain(), null, "main");
+            current.alive = true;
+
+            parent = null;
+            importables = new Array<>();
+            // TODO push all script which need
+            HTMLScriptElement element = (HTMLScriptElement)document.currentScript;
+            importables.push(element.src);
+        }
+        if (IS_WORKER_THREAD) {
+            onWorkerStart();
+        }
+    }
+
+    private final ThreadGroup threadGroup;
+    final int id;
+    private final Runnable target;
+    private Worker worker;
+    String name;
+    private int priority = 0;
+    private boolean _interrupted = false;
+    private boolean alive = false;
+    private boolean daemon = true;
+
+    public Thread() {
+        this(ThreadGroup.getMain(), (Runnable) null);
+    }
+
+    public Thread(Runnable target) {
+        this(ThreadGroup.getMain(), target);
+    }
+
+    public Thread(Runnable target, String name) {
+        this(ThreadGroup.getMain(), target, name);
+    }
+
+    public Thread(String name) {
+        this(ThreadGroup.getMain(), null, name);
+    }
+
+    public Thread(ThreadGroup group, Runnable target) {
+        this(group, target, "Thread-" + ID_COUNTER);
+    }
+
+    public Thread(ThreadGroup group, String name) {
+        this(group, null, name);
+    }
+
+    public Thread(ThreadGroup threadGroup, Runnable target, String name) {
+        this.threadGroup = threadGroup;
+        this.name = name;
+        this.target = target;
+        this.id = ID_COUNTER++;
+
+        if (threadGroup != null) {
+            array(threadGroup.childs).push(this);
+        }
+    }
+
+    Thread(ThreadGroup threadGroup, Runnable target, String name, int id) {
+        this.threadGroup = threadGroup;
+        this.name = name;
+        this.target = target;
+        this.id = id;
+
+        if (threadGroup != null) {
+            array(threadGroup.childs).push(this);
+        }
+    }
+
+
+    public static void sleep(long millis) {
+        // busy wait
+        double to = new Date().getTime() + millis;
+        while (new Date().getTime() < to) {
+            Thread.yield();
+        }
+    }
+
+    public static void sleep(long millis, int nanos) {
+        // busy wait
+        double to = performance.now() + millis + nanos / 1000000d;
+        while (performance.now() < to) {
+            Thread.yield();
+        }
+    }
+
+    public static void yield() {
+    }
+
+    public static boolean interrupted() {
+        if (current == null) return false;
+
+        boolean res = current._interrupted;
+        current._interrupted = false;
+        return res;
+    }
+
+    public static int activeCount() {
+        return current == null ? 0 : current.getThreadGroup().activeCount();
+    }
+
+    public static Thread currentThread() {
+        return current;
+    }
+
+    public static int enumerate(Thread[] tarray) {
+        return current == null ? 0 : current.getThreadGroup().enumerate(tarray);
+    }
+
+    public static void dumpStack() {
+        System.err.println(new Error().stack);
+    }
+
+    public ThreadGroup getThreadGroup() {
+        return threadGroup;
+    }
+
+    public String toString() {
+        return this.getClass().getName() + '[' + name + ',' + getPriority() + ',' + getThreadGroup().getName() + ']';
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isInterrupted() {
+        return _interrupted;
+    }
+
+    public void run() {
+        if (target != null) {
+            target.run();
+        }
+    }
+
+    public void interrupt() {
+        _interrupted = true;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setDaemon(boolean on) {
+    }
+
+    public void destroy() {
+        alive = false;
+        // TODO call the upper thread to terminate us.
+        if (parent != null) {
+            // parent.worker.postMessage({""});
+        }
+    }
+
+    public void start() {
+        if (worker != null) {
+            return;
+        }
+
+        alive = true;
+
+        worker = new Worker(URL.createObjectURL(new Blob(new Object[]{"self.onmessage = e => { importScripts.apply(self, e.data); java.lang.Thread.importables = e.data; }"}, new BlobPropertyBag() {{
+            $set("type", "text/javascript");
+        }})));
+        worker.onmessage = Thread::onMessage;
+        worker.onerror = Thread::onError;
+        worker.postMessage(importables);
+
+        Object threadGroupCopy = JSON.parse(JSON.stringify(ThreadGroup.getMain(), (key, obj) -> {
+            switch (key) {
+                case "parent":
+                case "threadGroup":
+                    return undefined;
+                case "target":
+                    return obj == null ? obj : obj.toString();
+            }
+            return obj;
+        }));
+
+        int currentId = id;
+        int parentId = current.id;
+        worker.postMessage(createCommand("threads", new def.js.Object() {{
+            $set("tree", threadGroupCopy);
+            $set("current", currentId);
+            $set("parent", parentId);
+            $set("maxgroup", ThreadGroup.ID_COUNTER);
+            $set("maxthread", Thread.ID_COUNTER);
+        }}));
+
+        worker.postMessage(createCommand("start", undefined));
+    }
+
+    private static def.js.Object createCommand(String command, Object data) {
+        def.js.Object commandObj = new def.js.Object();
+        commandObj.$set("command", command);
+        commandObj.$set("data", data);
+        return commandObj;
+    }
+
+    public static void onWorkerStart() {
+        self.onmessage = Thread::onMessage;
+        self.$set("onmessageerror", (Function<MessageEvent, def.js.Object>)(MessageEvent e) -> Thread.onMessageError(e));
+    }
+
+    public static def.js.Object onMessage(MessageEvent event) {
+        def.js.Object eventData = object(event.data);
+        def.js.Object commandData = eventData.$get("data");
+        switch (eventData.$get("command").toString()) {
+            case "threads": {
+                ThreadGroup.updateWholeTree(commandData.$get("tree"));
+                ThreadGroup main = ThreadGroup.getMain();
+                main.list();
+                Thread[] arr = new Thread[main.activeCount()];
+                main.enumerate(arr);
+
+                for (Thread thread : arr) {
+                    if (thread.id == (int) commandData.$get("current")) {
+                        current = thread;
+                    }
+                    if (thread.id == (int) commandData.$get("parent")) {
+                        parent = thread;
+                    }
+                }
+
+                ThreadGroup.ID_COUNTER = commandData.$get("maxgroup");
+                Thread.ID_COUNTER = commandData.$get("maxthread");
+
+                System.out.println("Current: " + currentThread());
+                System.out.println("Parent: " + parent);
+                break;
+            }
+            case "start": {
+                Thread.currentThread().run();
+                break;
+            }
+            default:
+                System.err.println("Got an unrecognized command: " + eventData.$get("command") + " with data: " + commandData);
+                break;
+        }
+        return self;
+    }
+
+    public static def.js.Object onMessageError(MessageEvent event) {
+        System.err.println("Got a message error :( " + event.data);
+        return self;
+    }
+
+    public static Object onError(Event event) {
+        System.err.println("Got a error :( ");
+        return self;
+    }
+
+    void updateThread(Thread nakedThread) {
+        this.name = nakedThread.name;
+        this.alive = nakedThread.alive;
+        this._interrupted = nakedThread._interrupted;
+        this.priority = nakedThread.priority;
+        this.daemon = nakedThread.daemon;
+    }
+}
---
+++ src/main/java/java/lang/ThreadGroup.java
@@ -0,0 +1,268 @@
+package java.lang;
+
+import java.util.function.Function;
+
+import static jsweet.util.Lang.*;
+
+public class ThreadGroup {
+    static int ID_COUNTER;
+    private static ThreadGroup MAIN;
+    private final ThreadGroup parent;
+    private final String name;
+    private final int id;
+
+    private ThreadGroup[] childGroups = new ThreadGroup[0];
+    public Thread[] childs = new Thread[0];
+    private boolean destroyed = false;
+
+    public static ThreadGroup getMain() {
+        if (MAIN == null) {
+            MAIN = new ThreadGroup(null, "main");
+        }
+        return MAIN;
+    }
+
+    public ThreadGroup(String name) {
+        this(getMain(), name);
+    }
+
+    public ThreadGroup(ThreadGroup parent, String name) {
+        this.parent = parent;
+        this.name = name;
+        this.id = ID_COUNTER++;
+
+        if (parent != null) {
+            array(parent.childGroups).push(this);
+        }
+    }
+
+    private ThreadGroup(ThreadGroup parent, String name, int id, boolean destroyed) {
+        this.parent = parent;
+        this.name = name;
+        this.id = id;
+        this.destroyed = destroyed;
+
+        if (parent != null) {
+            array(parent.childGroups).push(this);
+        }
+    }
+
+    public int activeCount() {
+        int result = childs.length;
+        for (ThreadGroup childGroup : childGroups) {
+            result += childGroup.activeCount();
+        }
+        return result;
+    }
+
+    public int activeGroupCount() {
+        int result = childGroups.length;
+        for (ThreadGroup childGroup : childGroups) {
+            result += childGroup.activeGroupCount();
+        }
+        return result;
+    }
+
+    public void destroy() {
+        ThreadGroup[] cp = new ThreadGroup[childGroups.length];
+        arraycopy(childGroups, 0, cp, 0, childGroups.length);
+
+        for (ThreadGroup childGroup : cp) {
+            childGroup.destroy();
+        }
+        for (Thread child : childs) {
+            child.destroy();
+        }
+        if (parent != null) {
+            array(parent.childGroups).splice(array(parent.childGroups).indexOf(this), 1);
+        }
+        this.destroyed = true;
+    }
+
+    public int enumerate(Thread[] list) {
+        return enumerate(list, true);
+    }
+
+    public int enumerate(Thread[] list, boolean recurse) {
+        if (recurse) {
+            return enumerate_inner(list, 0, t -> t.childs);
+        } else {
+            int threadsToCopy = Math.min(list.length, childs.length);
+            arraycopy(childs, 0, list, 0, threadsToCopy);
+            return threadsToCopy;
+        }
+    }
+
+    public int enumerate(ThreadGroup[] list) {
+        return enumerate(list, true);
+    }
+
+    public int enumerate(ThreadGroup[] list, boolean recurse) {
+        if (recurse) {
+            return enumerate_inner(list, 0, t -> t.childGroups);
+        } else {
+            int threadGroupsToCopy = Math.min(list.length, childGroups.length);
+            arraycopy(childGroups, 0, list, 0, threadGroupsToCopy);
+            return threadGroupsToCopy;
+        }
+    }
+
+    private <T> int enumerate_inner(T[] list, int fromIndex, Function<ThreadGroup, T[]> fromArray) {
+        if (fromIndex == list.length)
+            return fromIndex;
+
+        int threadsToCopy = Math.min(list.length - fromIndex, fromArray.apply(this).length);
+        arraycopy(fromArray.apply(this), 0, list, fromIndex, threadsToCopy);
+
+        int result = fromIndex + threadsToCopy;
+        for (ThreadGroup childGroup : childGroups) {
+            if (result == list.length)
+                return result;
+
+            result = childGroup.enumerate_inner(list, result, fromArray);
+        }
+        return result;
+    }
+
+    public int getMaxPriority() {
+        return Thread.MAX_PRIORITY;
+    }
+
+    public void setMaxPriority(int pri) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ThreadGroup getParent() {
+        return parent;
+    }
+
+    public void interrupt() {
+        for (ThreadGroup childGroup : childGroups) {
+            childGroup.interrupt();
+        }
+        for (Thread child : childs) {
+            child.interrupt();
+        }
+    }
+
+    public boolean isDaemon() {
+        return true;
+    }
+
+    public void setDaemon(boolean daemon) {
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public void list() {
+        list_indented("");
+    }
+
+    private void list_indented(String indention) {
+        System.out.println(indention + toString());
+        String childIndentation = indention + "    ";
+        for (Thread child : childs) {
+            System.out.println(childIndentation + child.toString());
+        }
+        for (ThreadGroup childGroup : childGroups) {
+            childGroup.list_indented(childIndentation);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getName() + "[name=" + name + ",maxpri=" + getMaxPriority() + ']';
+    }
+
+    public boolean parentOf(ThreadGroup g) {
+        return this == g || g.parent != null && this.parentOf(g.parent);
+    }
+
+
+    static void updateWholeTree(def.js.Object treeObject) {
+        ThreadGroup naked = any(treeObject);
+        getMain().updateTree(naked);
+    }
+
+    private void updateTree(ThreadGroup nakedObject) {
+        // childgroup
+        for (ThreadGroup nakedChildGroup : nakedObject.childGroups) {
+            ThreadGroup real = null;
+
+            for (ThreadGroup childGroup : childGroups) {
+                if (nakedChildGroup.id == childGroup.id) {
+                    real = childGroup;
+                    break;
+                }
+            }
+
+            if (real == null) {
+                real = new ThreadGroup(this, nakedChildGroup.name, nakedChildGroup.id, nakedChildGroup.destroyed);
+            }
+            real.updateTree(nakedChildGroup);
+        }
+
+        ThreadGroup[] cpChildGroup = new ThreadGroup[childGroups.length];
+        enumerate(cpChildGroup, false);
+
+        for (ThreadGroup childGroup : cpChildGroup) {
+            boolean found = false;
+            for (ThreadGroup nakedChildGroup : nakedObject.childGroups) {
+                if (childGroup.id == nakedChildGroup.id) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                childGroup.destroy();
+            }
+        }
+
+        // child
+        for (Thread nakedChild : nakedObject.childs) {
+            Thread real = null;
+
+            for (Thread child : childs) {
+                if (nakedChild.id == child.id) {
+                    real = child;
+                    break;
+                }
+            }
+
+            if (real == null) {
+                real = new Thread(this, new def.js.Function("return " + object(nakedChild).$get("target")).$apply(), nakedChild.name, nakedChild.id);
+            }
+            real.updateThread(nakedChild);
+        }
+
+        Thread[] cpChild = new Thread[childs.length];
+        enumerate(cpChild, false);
+
+        for (Thread child : cpChild) {
+            boolean found = false;
+            for (Thread nakedChild : nakedObject.childs) {
+                if (child.id == nakedChild.id) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                child.destroy();
+            }
+        }
+
+        // destroyed flag
+        this.destroyed = nakedObject.destroyed;
+    }
+
+    private void arraycopy(Object[] from, int fromStart, Object[] to, int toStart, int length) {
+        int deleteCount = Math.min(to.length - toStart, length);
+        int fromEnd = Math.min(from.length, fromStart + length);
+        $insert("Array.prototype.splice.apply(to, [toStart, deleteCount].concat(from.slice(fromStart, fromEnd)))");
+    }
+}
